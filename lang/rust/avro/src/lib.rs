9dcd8e31af229165e0e6e1d6f6d9655b57e777be
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2022-10-06 11:45:50 +0300

    AVRO-3631: Add serde serialize_with functions
    Those should be used for hinting the serialization process how to serialize a byte array to Value::(Bytes|Fixed)
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- lang/rust/avro/src/lib.rs
+++ lang/rust/avro/src/lib.rs
@@ -746,7 +746,7 @@ pub use reader::{
     from_avro_datum, read_marker, GenericSingleObjectReader, Reader, SpecificSingleObjectReader,
 };
 pub use schema::{AvroSchema, Schema};
-pub use ser::to_value;
+pub use ser::{avro_serialize_bytes, avro_serialize_fixed, to_value};
 pub use util::max_allocation_bytes;
 pub use writer::{to_avro_datum, GenericSingleObjectWriter, SpecificSingleObjectWriter, Writer};
 
